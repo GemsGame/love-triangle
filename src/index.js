0c868ac5c285b8b9b3fd5297754a9cb9f3886cdb
GemsGame <support@gems.cash>
2018-09-13 12:55:50 +0300

    triangle_algo2

--- src/index.js
+++ src/index.js
@@ -3,65 +3,23 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+    
+
+  for(var i = 0; i < preferences.length; i++) preferences[i]--;
+  var count = 0;
+   for(var i = 0; i < preferences.length; i++)
+  {
+      var s1= preferences[i];
+      var s2 = preferences[s1];
+      var s3 = preferences[s2];
+      
+      if(s3 == i && s1 != s2 && s2 != s3) 
+         {
+        count++;
+         }
+  }
+ return (count/ 3);
+    
 };
 
-
-
-
-
-
-
-/* старт ----------------------------------- */
-var arr = [2, 3, 1, 5, 6, 4, 2, 3, 1, 5, 6, 4, 2, 3, 1, 5, 6, 4];
-
-var elements = arr.length/3;
-
-var elements = Math.trunc(elements);
-//console.log (elements); //элементы помещаются 1 раз (3шт)
-arr.length; // длинна массива 5 элементов
-var del = elements*3;
-arr.length = del;
-
-
-var size = 3; //размер подмассива
-var subarray = []; //массив в который будет выведен результат.
-for (var i = 0; i <Math.ceil(arr.length/size); i++){
-    subarray[i] = arr.slice((i*size), (i*size) + size);
-}
-
-
-/*------------------------------------------ */
-
-//console.log(subarray); массив с треугольниками
-
-
-
-
-while (elements>0) {
-
-      
-    --elements;
-    console.log(elements); //айди для вставки в функцию... 0\1\2
-    var omg_tringle = true_triangle(subarray, elements);
-   
-  
-}
-
-
-  console.log("triangle:" omg_tringle);
-/* вычисление треугольников -------------------------- */
-    
-var triangle = 0; // 0/1/2/3/4/5/6
-
-
-function true_triangle (subarray, elements) {
-if (subarray[elements][0]< subarray[elements][1] && subarray[elements][1]> subarray[elements][2])
-{
-    
-    return triangle++;
-   
-}
-    
-}
-                                           
+                 
